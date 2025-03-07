95b8af2bfa78f34ddc9d5a310e7834ecd6ebf9ff
DHMIN0527 <eheh8734@naver.com>
2025-03-07 20:00:34 +0900

    first commit

---
+++ src/test/java/com/v02/concurrency/ConcurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.v02.concurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
